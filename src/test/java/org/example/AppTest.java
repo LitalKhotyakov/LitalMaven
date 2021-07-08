18b6821b8016997ab28b00af471a6001d88dd70f
khyk96 <khyk96@gmail.com>
2021-07-08 04:22:17 +0300

    3 commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +16,20 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+    }
+
+    @Test
+    public void shouldAReturnSum()
+    {
+        App myApp = new App();
+        assertEquals( myApp.testSum(8,5) , 13 );
+        assertEquals( myApp.testSum(8,8) , 16 );
+        assertEquals( myApp.testSum(2,5) , 7 );
     }
 }
