9b95b599b9c54392c061b45a864848c16495721b
khyk96 <khyk96@gmail.com>
2021-07-08 09:59:38 +0300

    3 commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,4 +14,14 @@ public class App
     public int testSum(int a, int b){
         return a + b;
     }
+
+    public String printMessage(String message){
+        return message;
+    }
+
+    public boolean testIntArray(int[] a, int[] b){
+        System.out.println("a eq b " + (a.length == b.length));
+        return a.length == b.length;
+    }
+
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -32,4 +32,32 @@ public class AppTest
         assertEquals( myApp.testSum(8,8) , 16 );
         assertEquals( myApp.testSum(2,5) , 7 );
     }
+
+    @Test
+    public void shouldPrintMessage()
+    {
+        App myApp1 = new App();
+        myApp1.printMessage("lital");
+    }
+
+    @Test
+    public void shouldAnswerEqualsArray()
+    {
+        App myApp2 = new App();
+        int [] a = new int[]{5, 8, 3};
+        int [] b = new int[]{5, 8, 3};
+        assertArrayEquals(a,b);
+    }
+
+    @Test
+    public void shouldAnswerEqualsArray2()
+    {
+        App myApp2 = new App();
+        int [] a = new int[]{5, 8, 3};
+        int [] b = new int[]{5, 8, 3};
+        int [] c = new int[]{5, 8, 3, 6, 3};
+        assertTrue(myApp2.testIntArray(a,b));
+        assertFalse(myApp2.testIntArray(a,c));
+    }
+
 }
